ea76c20770a989940c9512d49979e49e1d352869
Alexandre Oliveira <contato@alexandreo.com.br>
2020-03-22 10:51:56 -0300

    remove laravel illuminate

--- src/Correios/ResponseFactory/ConsultaCepResponseFactory.php
+++ src/Correios/ResponseFactory/ConsultaCepResponseFactory.php
@@ -23,14 +23,14 @@ class ConsultaCepResponseFactory
      */
     public function make($soapResponse)
     {
-        $this->response->id = data_get($soapResponse, 'return.id', 0);
-        $this->response->enderenco = data_get($soapResponse, 'return.end', null);
-        $this->response->bairro = data_get($soapResponse, 'return.bairro', null);
-        $this->response->complemento = data_get($soapResponse, 'return.complemento', null);
-        $this->response->complemento2 = data_get($soapResponse, 'return.complemento2', null);
-        $this->response->cep = data_get($soapResponse, 'return.cep', null);
-        $this->response->uf = data_get($soapResponse, 'return.uf', null);
-        $this->response->cidade = data_get($soapResponse, 'return.cidade', null);
+        $this->response->id = $soapResponse->return->id ?? 0;
+        $this->response->enderenco = $soapResponse->return->end ?? null;
+        $this->response->bairro = $soapResponse->return->bairro ?? null;
+        $this->response->complemento = $soapResponse->return->complemento ?? null;
+        $this->response->complemento2 = $soapResponse->return->complemento2 ?? null;
+        $this->response->cep =  $soapResponse->return->cep ?? null;
+        $this->response->uf = $soapResponse->return->uf ?? null;
+        $this->response->cidade = $soapResponse->return->cidade ?? null;
 
         return $this->response;
     }
